946abe69a22bc708f54b81be87391fd9e3f6e08e
SunnyD <36974912+damonlemmons@users.noreply.github.com>
2018-03-29 11:46:29 -0700

    Update README.js

--- README.js
+++ README.js
@@ -1,5 +1,3 @@
-# Objects-Car-Challenge
-
 // Story 1
 // Create a function that creates car objects which have a make, model year, and color.
 
@@ -13,20 +11,46 @@
 // color
 // Add a function to the object that is being returned that concatenates the make, year and color.
 
+
+/*The car can accelerate, brake, and tell you its speed.
+
+Challenges:
+
+Add a speed variable to the closure of the car object. Add the following functions to the object:
+getSpeed: returns current speed
+accelerate: increases the speed by 10mph (but does not return anything)
+brake: decreases the speed by 7mph (but does not return anything)*/
+
+
+
 function carMaker(make, year, color) {  // begin createNewPerson
+  var speed = 0
   return {                        // begin object
     make: make,
     year: year,
     color: color,
     getData: function() {         // begin function
       return "Your car is a " + this.year + " " + this.color + " " + this.make 
-    }                             // end function
+    },
+    getSpeed: function() {
+      return speed
+    },
+    accelerate: function() {
+      speed = speed + 10
+    },
+    brake: function() {
+      speed = speed - 7
+    }                              // end function
   };                              // end object
 }                                 // end createNewPerson
 
 var car1 = carMaker ("Ford", 2003, "white");
-console.log(car1.getData())
-
+console.log(car1.accelerate())
+console.log(car1.brake())
+console.log(car1.getSpeed())
+console.log(car1.accelerate())
+console.log(car1.brake())
+console.log(car1.getSpeed())
 
 
 carMaker ("Ford", 2003, "white")
